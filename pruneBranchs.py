0aade3b2a73abe6fe54a8403a9b478c5a5c30d34
ciro.f.c.98@gmail.com <ciro.f.c.98@gmail.com>
2021-09-08 13:58:27 -0300

    Updated script to prune branchs

--- pruneBranchs.py
+++ pruneBranchs.py
@@ -1,22 +1,33 @@
-import pdb
-import sys
+import os, shutil, sys
+from git import *
 
-import customFilters
-from git import Repo, remote
+def clone_repo (remote_url, local_path, branch):
+    print("Cloning repo %s" % remote_url)
+    if os.path.isdir(local_path) is True:
+        shutil.rmtree(local_path)
+    Repo.clone_from(remote_url, local_path, branch=branch)
 
-def pruneBranchs(repo):
-    
-    # Retrieving repository
-    re = Repo(repo)
-    # Getting heads
-    repo_heads = re.heads 
+def delete_remote_branches(remote_url, main_branch, branch):
+    cur_dir = os.path.dirname(__file__)
+    local_path=f'{cur_dir}/repoToPrune'
 
-    # Filter unused branchs
-    branch_names = [x for x in map(customFilters.filter_names, repo_heads) if x is not None]
-    #remote.delete(Repo, branch_names, force=True)
+    clone_repo(
+        remote_url=remote_url,
+        local_path=local_path,
+        branch=main_branch
+    )
+    repo = Repo(local_path)                  
+    assert not repo.bare   
+    remote = repo.remote()
+    for repo_branch in repo.references:
+        delete = False   
+        # for branch in branches:
+        if repo_branch.name.find(branch) >= 0:
+            print(f'deleting remote branch: {repo_branch.remote_head}')
+            remote.push(refspec=(f':{repo_branch.remote_head}'))
 
-def delete_branch(branches):
-    print("deleting remote branch: %s" % branches.remote_head)
-    remote.push(refspec=(":%s" % branches.remote_head)) # remote_head = the branch you want to delete Example: "origin/my-branch"
-
-pruneBranchs(sys.argv[1])
+# Usage:
+#       $1 = github link to the repo
+#       $2 = main branch
+#       $3 = branch prefix
+delete_remote_branches(sys.argv[1], sys.argv[2], sys.argv[3])
